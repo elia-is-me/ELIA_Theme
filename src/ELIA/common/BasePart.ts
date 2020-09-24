796f829809fa06763ec2ace22ee15134b94a6c58
elia.of.lt <elia.of.little@gmail.com>
2020-09-24 17:01:51 +0800

    setPosition can also update children's position

--- src/ELIA/common/BasePart.ts
+++ src/ELIA/common/BasePart.ts
@@ -55,9 +55,6 @@ export interface IInjectableCallbacks {
 	on_playback_edited?: () => void;
 	on_playback_pause?: (isPaused?: boolean) => void;
 	on_playback_new_track?: (metadb?: IFbMetadb) => void;
-	// on_playlist_items_added?:(playlistIndex?: number) => void;
-	// on_playlist_items_removed?:(playlistIndex?: number) => void;
-	// on_playlist_items_reordered?: (playlistIndex?: number) => void;
 	on_playlists_changed?: () => void;
 	on_playlist_switch?: () => void;
 	on_metadb_changed?: (metadbs?: IFbMetadbList, fromhook?: boolean) => void;
@@ -68,23 +65,36 @@ export abstract class Component implements IBoxModel, ICallbacks {
 	private _visible: boolean = true;
 	private _shouldUpdateOnInit = true;
 	private _shouldSortChildren = true;
+
+	// offset to smp_panel's left;
 	x: number = 0;
+
+	// offset to smp_panel's top;
 	y: number = 0;
+
+	// offset to parent part's left;
+	private _clientX: number = 0;
+
+	// offset to parent part's top;
+	private _clientY: number = 0;
+
 	private _zIndex: number = 0;
+
 	get z() {
 		return this._zIndex;
 	}
+
 	set z(zIndex: number) {
 		if (this.z !== zIndex) {
 			this._zIndex = zIndex;
 			this.parent && (this.parent._shouldSortChildren = true);
 		}
 	}
+
 	width: number = 0;
 	height: number = 0;
 	parent: Component;
 	children: Component[] = [];
-	manager: UserInterface;
 	paddings: IPaddings = {
 		top: 0,
 		bottom: 0,
@@ -101,10 +111,10 @@ export abstract class Component implements IBoxModel, ICallbacks {
 	/**
 	 * 'on_init' 考虑的是当一个部件从隐藏切换到显示后，有一个默认的方法更新/初始化该部件。这么做是否合理现在值得怀疑。
 	 */
-	on_init() {}
-	on_paint(gr: IGdiGraphics) {}
-	on_size() {}
-	on_click(x: number, y: number) {}
+	on_init() { }
+	on_paint(gr: IGdiGraphics) { }
+	on_size() { }
+	on_click(x: number, y: number) { }
 	addChild(node: Component) {
 		if (!(node instanceof Component)) {
 			throw new Error("Component.addChild: Invalid param.");
@@ -139,6 +149,7 @@ export abstract class Component implements IBoxModel, ICallbacks {
 			this._shouldUpdateOnInit = true;
 		}
 	}
+
 	trace(x: number, y: number) {
 		return (
 			this.isVisible() &&
@@ -148,10 +159,20 @@ export abstract class Component implements IBoxModel, ICallbacks {
 			y <= this.y + this.height
 		);
 	}
+
+/**
+ * generally children's position should be updated both when their parent's
+ * position or size changed, so use `setBoundry' or `setPosition' in a parent's
+ * `on_size' method.
+ */
 	setBoundary(x: number, y: number, width: number, height: number) {
 		let visibleBefore_ = this.isVisible();
 		this.x = x;
 		this.y = y;
+		if (this.parent) {
+			this._clientX = this.x - this.parent.x;
+			this._clientY = this.y - this.parent.y;
+		}
 		if (width != null) {
 			this.width = width;
 			this.height = height;
@@ -195,13 +216,29 @@ export abstract class Component implements IBoxModel, ICallbacks {
 			let height = arg_1.height || this.height;
 			this.setSize(width, height);
 		}
-		return this;
 	}
 
-	setPosition(x: number, y: number) {
-		this.x = x;
-		this.y = y;
-		this.on_size && this.on_size();
+	/**
+	 * setPosition will not only update its self's position, but also its
+	 * children's position offset to smp_panel, but children's position offset to
+	 * parent will keep.
+	 */
+	setPosition(x: number, y: number): void;
+	setPosition(pos: { x?: number, y?: number }): void;
+	setPosition(x: number | { x?: number; y?: number }, y?: number) {
+		if (typeof x == "number") {
+			this.x = x;
+			this.y = y;
+			if (this.parent) {
+				this._clientX = this.x - this.parent.x;
+				this._clientY = this.y - this.parent.y;
+			}
+			this.children.forEach(child => {
+				child.setPosition(this.x + child._clientX, this.y + child._clientY);
+			})
+		} else {
+			this.setPosition(isNaN(x.x) ? this.x : x.x, isNaN(x.y) ? this.y : x.y);
+		}
 	}
 
 	didUpdateOnInit() {
@@ -216,10 +253,9 @@ export abstract class Component implements IBoxModel, ICallbacks {
 		return this._shouldUpdateOnInit;
 	}
 
-	onNotifyData?(str: string, info: any) {}
+	onNotifyData?(str: string, info: any) { }
 
 	repaint() {
-		// window.RepaintRect(this.x, this.y, this.width, this.height);
 		window.Repaint();
 	}
 }
