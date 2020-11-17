81d73e7051376ff564aae8253906911c16ff6034
elia.of.lt <elia.of.little@gmail.com>
2020-11-17 20:35:41 +0800

    rename outfile to 'elia-theme.js' & clean gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,8 @@ const gulp = require("gulp");
 const browserify = require("browserify");
 const source = require("vinyl-source-stream");
 const tsify = require("tsify");
-//const watchify = require("watchify");
-//const babelify = require("babelify");
 
 gulp.task("default", function () {
-    // return browserify({
-    //     basedir: "./src",
-    //     debug: true,
-    //     entries: ["ELIA/main.ts" ],
-    //     paths: ["./src"],
-    //     cache: {},
-    //     packageCache: {}
-    // })
-    //     .plugin(tsify, { target: "es2017", include: ["src"]})
-    //     .bundle()
-    //     .pipe(source("bundle.js"))
-    //     .pipe(gulp.dest("out"));
     return browserify({
         "basedir": ".",
         "entries": "src/ELIA/main.ts",
@@ -30,12 +16,8 @@ gulp.task("default", function () {
                 "target": "ES2018",
                 "baseUrl": "./",
             })
-        // .transform(babelify, {
-        //     // "presets": ["env"],
-        //     // "extensions": [".ts"]
-        // })
         .on('error', function (error) { console.error(error.toString()); })
         .bundle()
-        .pipe(source("elia-theme-main.js"))
+        .pipe(source("elia-theme.js"))
         .pipe(gulp.dest("out"));
 });
